993a128f0524f2214473b02fb1534f2911408782
TingFeng36 <2450978258@qq.com>
2024-10-07 22:41:00 +0800

    Delete Oct-vacation/Task4/bul_on_off.js

--- Oct-vacation/Task4/bul_on_off.js
+++
@@ -1,9 +0,0 @@
-function changeImage() {
-  element = document.getElementById('myimage')
-  if (element.src.match("bulbon")) {
-    element.src = "pic_bulboff.gif";
-  }
-  else {
-    element.src = "pic_bulbon.gif";
-  }
-}
